12fd0c78712370706f1f1439cf119ec9790b4618
fluktar <tyc.pawel@gmail.com>
2023-11-15 15:09:16 +0100

    add todo.model.js

--- data/database.js
+++ data/database.js
@@ -1,14 +1,13 @@
-
-      // decyduj który potrzebujesz sql czy mongo
-      //tutaj jest sql
-    //   const mysql = require("mysql2/promise");
-	// 		const pool = mysql.createPool({
-	// 		  host: "localhost",
-	// 		  databasea: "blog",
-	// 		  user: "root",
-	// 		  password: "Sojokotojo1@3",
-	// 		});
-	// 		module.exports = pool;
+// decyduj który potrzebujesz sql czy mongo
+//tutaj jest sql
+//   const mysql = require("mysql2/promise");
+// 		const pool = mysql.createPool({
+// 		  host: "localhost",
+// 		  databasea: "blog",
+// 		  user: "root",
+// 		  password: "Sojokotojo1@3",
+// 		});
+// 		module.exports = pool;
 // ---------------------------------------------------------------
 //tutaj jest mongodb
 const mongodb = require("mongodb");
@@ -16,7 +15,7 @@ const MongoClient = mongodb.MongoClient;
 const password = require("./password");
 const url = `mongodb+srv://fluktar:${password}@cluster0.xw4ekbx.mongodb.net/?retryWrites=true&w=majority`;
 let database;
-async function connect() {
+async function initDb() {
   const client = await MongoClient.connect(url);
   database = client.db("todo_app");
 }
@@ -28,7 +27,6 @@ function getDb() {
   return database;
 }
 module.exports = {
-  connectToDatabase: connect,
+  initDb: initDb,
   getDb: getDb,
 };
-
