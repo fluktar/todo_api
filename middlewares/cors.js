04ffd10a09a5df02776ffa325b5dd66344ddb093
fluktar <tyc.pawel@gmail.com>
2023-11-22 13:47:10 +0100

    add to cors.js method options

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,10 @@
-function enableCors(req, res, next){
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-res.setHeader("Access-Control-Allow-Headers", 'Content-Type')
-next();
+function enableCors(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS, CORS, PATCH"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
 }
 module.exports = enableCors;
